15f0003ca5fea5fb2448b1063c3c8b558da586fd
Matthew Parks <matthewparks373@gmail.com>
2021-03-28 23:13:28 -0400

    serialized abbrev and make multiprocess work

--- parsing.py
+++ parsing.py
@@ -6,6 +6,7 @@ import pandas
 # os.environ['PATH'] += "C:/Program Files/R/R-4.0.4/bin/x64;"
 import rpy2.robjects as robjects
 import spacy
+from spacy.language import Language
 from rpy2.robjects import pandas2ri
 # noinspection PyUnresolvedReferences
 from scispacy.abbreviation import AbbreviationDetector
@@ -38,6 +39,27 @@ def iter_row(input_df):
         yield str(row['full_text']), {"cord_uid": row["cord_uid"], "type": "full_text"}
 
 
+@Language.component("serialize_abbreviation")
+def replace_abbrev_with_json(spacy_doc):
+    # https://github.com/allenai/scispacy/issues/205#issuecomment-597273144
+    # Code is modified to work as a component
+    new_abbrevs = []
+    for short in spacy_doc._.abbreviations:
+        short_text = short.text
+        short_start = short.start
+        short_end = short.end
+        long = short._.long_form
+        long_text = long.text
+        long_start = long.start
+        long_end = long.end
+        serializable_abbr = {"short_text": short_text, "short_start": short_start, "short_end": short_end,
+                             "long_text": long_text, "long_start": long_start, "long_end": long_end}
+        short._.long_form = None
+        new_abbrevs.append(serializable_abbr)
+    spacy_doc._.abbreviations = new_abbrevs
+    return spacy_doc
+
+
 def get_abbrev(pipeline, text_df, file_name, compress=False) -> None:
     # Given the spacy nlp and an pandas dataframe. Writes the results into file_name.
     print("Finding abbreviations")
@@ -63,11 +85,11 @@ def abbrev_iter(pipeline, test_iter):
 def abbrev_doc_iter(doc, context):
     found_abbrevs = defaultdict(str)
     for abbrev in doc._.abbreviations:
-        if abbrev._.long_form not in found_abbrevs:
-            found_abbrevs[abbrev._.long_form] = abbrev.text
+        if abbrev["long_text"] not in found_abbrevs:
+            found_abbrevs[abbrev["long_text"]] = abbrev["short_text"]
 
-    for definition, text_abrv in found_abbrevs.items():
-        yield f"{context['cord_uid']},{context['type']},{text_abrv},\"{definition}\"\n"
+    for long_text, short_text in found_abbrevs.items():
+        yield f"{context['cord_uid']},{context['type']},{short_text},\"{long_text}\"\n"
 
 
 def pos_doc_iter(doc, context):
@@ -106,7 +128,7 @@ def get_file(file_name, compress=False):
         return open(file_name, 'wt', encoding='utf-8')
 
 
-def run(pipeline, text_df, dependency_file_name, pos_file_name, compress=False) -> None:
+def run(pipeline, text_df, dependency_file_name, pos_file_name, abbreviation_file_name, compress=False) -> None:
     print("Finding dependencies")
     dep_file = get_file(dependency_file_name, compress)
     dep_file.write("cord_uid,type,sentence,text,dep,pos,head_text,head_pos,children\n")
@@ -115,11 +137,17 @@ def run(pipeline, text_df, dependency_file_name, pos_file_name, compress=False) 
     pos_file_ = get_file(pos_file_name, compress)
     pos_file_.write("cord_uid,type,text\n")
 
+    print("Find abbreviation")
+    abbrev_file = get_file(abbreviation_file_name, compress)
+    abbrev_file.write("cord_uid,abbreviation,full_definition\n")
+
     for doc, context in pipeline.pipe(iter_row(text_df), as_tuples=True, batch_size=BATCH_SIZE, n_process=N_PROCESS):
         for row in dependencies_doc_iter(doc, context):
             dep_file.write(row)
         for row in pos_doc_iter(doc, context):
             pos_file_.write(row)
+        for row in abbrev_doc_iter(doc, context):
+            abbrev_file.write(row)
 
     dep_file.close()
     pos_file_.close()
@@ -130,6 +158,8 @@ if __name__ == "__main__":
     # spacy.require_gpu()
     # Note to self: do not turn off tok2vec because its needed for sentences
     nlp = spacy.load("en_core_sci_sm", exclude=['ner'])
+    nlp.add_pipe("abbreviation_detector")  # load this pipeline before running get_abrv
+    nlp.add_pipe("serialize_abbreviation", after="abbreviation_detector")
 
     # If you want it to be faster you can remove the parser
     # nlp = spacy.load("en_core_sci_sm", exclude=['parser', 'ner', 'tok2vec'])
@@ -139,12 +169,12 @@ if __name__ == "__main__":
 
     # cProfile.run('get_dependencies(nlp, df, "data/dependencies.csv")')
 
-    run(nlp, df, "data/dependencies.csv", "data/pos_tagged_text.csv")
+    run(nlp, df, "data/dependencies.csv", "data/pos_tagged_text.csv", "data/found_abbreviations.csv")
 
     # WARNING: Do not run abbreviation_detector with the other functions, it does not play nice with mutliple processes
     # Manually removing it from the pipeline doesn't work either
     # Add the pipe after you run the other two
     # I think it's because it's scispacy's stuff not spacy
-    nlp.add_pipe("abbreviation_detector")  # load this pipeline before running get_abrv
-    get_abbrev(nlp, df, "data/found_abbreviations.csv")
+    #nlp.add_pipe("abbreviation_detector")  # load this pipeline before running get_abrv
+    #get_abbrev(nlp, df, "data/found_abbreviations.csv")
     # nlp.remove_pipe("abbreviation_detector")
